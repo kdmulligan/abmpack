8cad2b5424c95e2319d22718f7d8a3183b67bd86
Kailey Henkle <kdmulligan@uiowa.edu>
2025-05-28 14:50:31 -0500

    add second argument to update avail rooms func

--- R/update_available_rooms.R
+++ R/update_available_rooms.R
@@ -1,4 +1,4 @@
-utils::globalVariables(c("hcup_id_vec"))
+utils::globalVariables(c("hcupids_vec"))
 
 #' @title Update list of available rooms based on occupancy
 #'
@@ -6,21 +6,22 @@ utils::globalVariables(c("hcup_id_vec"))
 #' object from the environment. Additionally, it has no return object because
 #' it replaces icu_avail and non_avail in the environment.
 #' @param rm_list list object with room info
+#' @param hcupids_vec vector of hcup ids
 #'
 #' @return Data frame with number of rows equal to the number of patient moves per day
 #' @export
 
-update_available_rooms = function(rm_list) {
+update_available_rooms = function(rm_list, hcupids_vec) {
   avail_idx = rm_list$rid_uniq[!(rm_list$rid_uniq) %in% (rm_list$occup@i + 1)]
-  icu_avail_temp <- vector("list", length(hcup_id_vec))
-  # non_avail_temp <- vector("list", length(hcup_id_vec))
-  names(icu_avail_temp) <- as.character(hcup_id_vec)
-  # names(non_avail_temp) <- as.character(hcup_id_vec)
+  icu_avail_temp <- vector("list", length(hcupids_vec))
+  # non_avail_temp <- vector("list", length(hcupids_vec))
+  names(icu_avail_temp) <- as.character(hcupids_vec)
+  # names(non_avail_temp) <- as.character(hcupids_vec)
   non_avail_temp <- icu_avail_temp
-  for(j in 1:length(hcup_id_vec)) {
+  for(j in 1:length(hcupids_vec)) {
     # index: facility, ICU/NON, available
-    icu_avail_temp[[j]] <- rm_list$rid_uniq[(rm_list$hcup_id == hcup_id_vec[j]) & (rm_list$icu == 1) & (rm_list$rid_uniq %in% avail_idx)]
-    non_avail_temp[[j]] <- rm_list$rid_uniq[(rm_list$hcup_id == hcup_id_vec[j]) & (rm_list$icu == 0) & (rm_list$rid_uniq %in% avail_idx)]
+    icu_avail_temp[[j]] <- rm_list$rid_uniq[(rm_list$hcup_id == hcupids_vec[j]) & (rm_list$icu == 1) & (rm_list$rid_uniq %in% avail_idx)]
+    non_avail_temp[[j]] <- rm_list$rid_uniq[(rm_list$hcup_id == hcupids_vec[j]) & (rm_list$icu == 0) & (rm_list$rid_uniq %in% avail_idx)]
   }
   ## original
   # assign("icu_avail", icu_avail_temp, envir = .GlobalEnv)
