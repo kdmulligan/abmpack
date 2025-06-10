ff3c50862c9c547c58113c8ced7ec39768bc2560
Kailey Henkle <kdmulligan@uiowa.edu>
2025-06-10 15:25:52 -0500

    add print to draw_CDI_los fn

--- R/draw_CDI_los.R
+++ R/draw_CDI_los.R
@@ -45,6 +45,9 @@ draw_CDI_los <- function(dist = CDI_los_dist_mdc_tran6days, current_day, transfe
   out <- mapply(
     function(arg1, arg2, arg3, arg4, arg5)
       if (arg1 < max(which(dist[[arg2]][[paste0("mdc_", arg3)]] != 0)) - 1) {
+        print(paste0("current day: ", arg1, "; transfer: ", arg2, "; mdc: ", arg3,
+                     "; hcup_los: ", arg4, "; min facility/dist: ", arg5
+                     ))
         sample(
           x = (arg1 + 1):arg5,
           size = 1,
