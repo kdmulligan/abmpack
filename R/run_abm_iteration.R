048a5dd003ff1185d419bee3832eb701f452d31a
Kailey Henkle <kdmulligan@uiowa.edu>
2025-06-24 14:19:05 -0500

    add browser

--- R/run_abm_iteration.R
+++ R/run_abm_iteration.R
@@ -758,6 +758,7 @@ run_abm_iteration <- function(n_days = 72,
       # 7c: get queue patients to admit
       # queue_viz_keys_df
       ## adjust LOS for symp queue patients
+      browser()
       adj_queue_los_df = queue_viz_keys_df |>
         filter(patid %in% new_symp_pat$patid) |>
         inner_join(hcup, join_by(patid, viz_key, hcup_id, adrgriskmortality)) |>
