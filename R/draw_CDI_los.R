be5dd0dcc9a66e6c7bd0a735cfd9563411018467
Kailey Henkle <kdmulligan@uiowa.edu>
2025-10-13 21:47:19 -0500

    add condition in case all prob in los_dist are equal to zero. then draw equally likely

--- R/draw_CDI_los.R
+++ R/draw_CDI_los.R
@@ -51,14 +51,21 @@ draw_CDI_los <- function(dist = CDI_los_dist_mdc_tran6days, current_day, transfe
         arg5
       } else if (arg1 == arg4 & arg1 == arg5) {
         arg1 + 1
-      } else if (arg1 < (max(which(dist[[arg2]][[paste0("mdc_", arg3)]] != 0)) - 1)) {
-        # print(paste0("opt2.  day: ", arg1, "; transfer: ", arg2, "; mdc: ", arg3,
-                     # "; hcup_los: ", arg4, "; max_to_use: ", arg5))
-        sample(
+      } else if (
+        arg1 < (max(which(dist[[arg2]][[paste0("mdc_", arg3)]] != 0)) - 1)) {
+        if(any((dist[[arg2]][[paste0("mdc_", arg3)]][(arg1 + 1):arg5]) != 0)) {
+          sample(
             x = (arg1 + 1):arg5,
             size = 1,
             prob = dist[[arg2]][[paste0("mdc_", arg3)]][(arg1 + 1):arg5]
           )
+        } else{
+          sample(
+            x = (arg1 + 1):arg5,
+            size = 1
+          )
+        }
+
       } else {
         # print("opt3")
         max(arg1 + 1, arg4)
